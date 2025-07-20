5df3e6790fafd9f005012f4a9eb0258afa7bf88e
Juan David Palacios <79817702+juandp1@users.noreply.github.com>
2025-07-20 17:33:05 -0500

    another remove of "/"

--- swintransformer/model.py
+++ swintransformer/model.py
@@ -162,12 +162,12 @@ class SwinTransformerBlock(tf.keras.layers.Layer):
         assert 0 <= self.shift_size < self.window_size, "shift_size must in 0-window_size"
         self.prefix = prefix
 
-        self.norm1 = norm_layer(epsilon=1e-5, name=f'{self.prefix}/norm1')
+        self.norm1 = norm_layer(epsilon=1e-5, name=f'{self.prefix}_norm1')
         self.attn = WindowAttention(dim, window_size=(self.window_size, self.window_size), num_heads=num_heads,
                                     qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, prefix=self.prefix)
         self.drop_path = DropPath(
             drop_path_prob if drop_path_prob > 0. else 0.)
-        self.norm2 = norm_layer(epsilon=1e-5, name=f'{self.prefix}/norm2')
+        self.norm2 = norm_layer(epsilon=1e-5, name=f'{self.prefix}_norm2')
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
                        drop=drop, prefix=self.prefix)
@@ -197,7 +197,7 @@ class SwinTransformerBlock(tf.keras.layers.Layer):
             attn_mask = tf.where(attn_mask != 0, -100.0, attn_mask)
             attn_mask = tf.where(attn_mask == 0, 0.0, attn_mask)
             self.attn_mask = tf.Variable(
-                initial_value=attn_mask, trainable=False, name=f'{self.prefix}/attn_mask')
+                initial_value=attn_mask, trainable=False, name=f'{self.prefix}_attn_mask')
         else:
             self.attn_mask = None
 
@@ -298,7 +298,7 @@ class BasicLayer(tf.keras.layers.Layer):
                                            drop_path_prob=drop_path_prob[i] if isinstance(
                                                drop_path_prob, list) else drop_path_prob,
                                            norm_layer=norm_layer,
-                                           prefix=f'{prefix}/blocks{i}') for i in range(depth)])
+                                           prefix=f'{prefix}_blocks{i}') for i in range(depth)])
         if downsample is not None:
             self.downsample = downsample(
                 input_resolution, dim=dim, norm_layer=norm_layer, prefix=prefix)
